cd8d07b21bf71e7decfb0615a03f90fe153bfd75
Seksak_aranchot <93124386+BKKStudio@users.noreply.github.com>
2023-08-04 14:24:52 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,16 @@
-
+"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function RootLayout({ children }) {
-
+  useEffect(()=>{
+    import('bootstrap/dist/js/bootstrap');
+  },[])
   
   return (
     <html lang="en">
