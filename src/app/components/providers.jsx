2ef879d78a2d71374b9291be2da40dd8ce55b205
Seksak_aranchot <93124386+BKKStudio@users.noreply.github.com>
2023-08-09 21:54:26 +0700

    first commit

--- src/app/components/providers.jsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { ThemeProvider } from "next-themes"
-
-const Providers = ({children}) => {
-    return(
-        <ThemeProvider>{children}</ThemeProvider>
-    )
-}
-export default Providers
