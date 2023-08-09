90da24ef9c86f6e531f4e9c630590b33009a1054
Seksak_aranchot <93124386+BKKStudio@users.noreply.github.com>
2023-08-09 21:56:40 +0700

    first commit

--- src/app/components/Alladmin.jsx
+++ src/app/components/Alladmin.jsx
@@ -2,8 +2,9 @@
 import React, { useState, useEffect } from "react";
 
 const getAlluser = async () =>{
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     try {
-        const res = await fetch(`/api/users/alluser`, {
+        const res = await fetch(`${apiUrl}/api/users/alluser`, {
           cache: "no-store",
         });
         if (!res.ok) {
